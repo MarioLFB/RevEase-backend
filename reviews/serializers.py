35197108fe00b9f1ade85ce5b68d92e846f24d2d
Mario Borges <popesco19@hotmail.com>
2024-09-16 19:07:46 +0100

    Add serializers for Product, Review, Comment, Like, and Follower models

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,2 +1,33 @@
 from rest_framework import serializers
 from .models import Product, Review, Comment, Like, Follower
+
+
+# Product Serializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+# Review Serializer
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+# Comment Serializer
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+# Like Serializer
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = '__all__'
+
+# Follower Serializer
+class FollowerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follower
+        fields = '__all__'
