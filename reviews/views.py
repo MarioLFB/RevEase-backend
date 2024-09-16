0576e2b33c50b58d1672fd6350ee7189f0a323c8
Mario Borges <popesco19@hotmail.com>
2024-09-16 19:16:58 +0100

    Refactor views.py in reviews app to use viewsets and add serializers for Product, Review, Comment, Like, and Follower models

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Review, Comment, Like, Follower
+from .serializers import ProductSerializer, ReviewSerializer, CommentSerializer, LikeSerializer, FollowerSerializer
 
-# Create your views here.
+
+# Product ViewSet
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Review ViewSet
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
