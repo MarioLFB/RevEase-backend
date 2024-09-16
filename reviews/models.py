c35ff371f0235b175276785136f2fcc0dbfa3122
Mario Borges <popesco19@hotmail.com>
2024-09-16 18:45:25 +0100

    Add Product and Review models to reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Model Product
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -9,4 +10,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+    
+
+# Model Review
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return f"Review for {self.product.name} by {self.author.username}"
 
