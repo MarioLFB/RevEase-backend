a813a52fd04abef24740f01bd2644adad0fba741
Mario Borges <popesco19@hotmail.com>
2024-09-16 19:32:09 +0100

    Refactor reviews app URLs to use viewsets and serializers for all models

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,1 +1,14 @@
 from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, ReviewViewSet, CommentViewSet, LikeViewSet, FollowerViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'likes', LikeViewSet)
+router.register(r'followers', FollowerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
