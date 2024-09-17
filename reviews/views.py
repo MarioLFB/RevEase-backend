26493ea5a778e7bb1d5f4e539d2d252e2da6c51b
Mario Borges <popesco19@hotmail.com>
2024-09-17 18:41:22 +0100

    Refactor viewsets to include filtering and ordering capabilities

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .models import Product, Review, Comment, Like, Follower
 from .serializers import ProductSerializer, ReviewSerializer, CommentSerializer, LikeSerializer, FollowerSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 # Product ViewSet
@@ -9,6 +11,10 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['category', 'price']
+    ordering_fields = ['price', 'created_at']
+    ordering = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -19,6 +25,10 @@ class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['product', 'rating']
+    ordering_fields = ['created_at', 'rating']
+    ordering = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -29,6 +39,10 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['review']
+    ordering_fields = ['created_at']
+    ordering = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -39,6 +53,10 @@ class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['review']
+    ordering_fields = ['created_at']
+    ordering = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -49,6 +67,10 @@ class FollowerViewSet(viewsets.ModelViewSet):
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['follower', 'following']
+    ordering_fields = ['follower', 'following']
+    ordering = ['follower']
 
     def perform_create(self, serializer):
         serializer.save(follower=self.request.user)
