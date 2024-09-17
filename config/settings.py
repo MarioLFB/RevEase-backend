332500f91dd0a2615a006efa2dd8a1a99298c17e
Mario Borges <popesco19@hotmail.com>
2024-09-17 18:27:29 +0100

    Add django_filters to INSTALLED_APPS and configure REST framework pagination and filtering

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
 
     # Third party apps
     'reviews',
+
+    # Filter Django for REST framework
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
@@ -39,6 +42,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 MIDDLEWARE = [
