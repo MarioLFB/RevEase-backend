7738291298a89bc172d5900f5f241280cce5bad1
Mario Borges <popesco19@hotmail.com>
2024-09-19 19:41:40 +0100

    Refactor authentication views to include logout functionality

--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -18,3 +20,15 @@ class RegisterView(generics.CreateAPIView):
             return Response({"token": token.key}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            token.delete()
+            return Response({"message": "Logout done!"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"error": "Token not found!"}, status=status.HTTP_400_BAD_REQUEST)
