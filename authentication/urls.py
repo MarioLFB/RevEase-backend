94f94e64ae99d6cbeb845120ed9d0818d15a1887
Mario Borges <popesco19@hotmail.com>
2024-09-20 18:01:17 +0100

    Refactor authentication views to include login functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LogoutView
+from .views import RegisterView, LogoutView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from .serializers import RegisterSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -32,3 +33,18 @@ class LogoutView(APIView):
             return Response({"message": "Logout done!"}, status=status.HTTP_200_OK)
         except Token.DoesNotExist:
             return Response({"error": "Token not found!"}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Wrong credentials"}, status=status.HTTP_400_BAD_REQUEST)
