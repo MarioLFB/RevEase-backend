3686521b7a77fcf8abea09f5318edc8894433809
Mario Borges <popesco19@hotmail.com>
2024-09-30 20:05:57 +0100

    Refactor serializers and views so that user can like and unlike us comment properly

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,15 +2,13 @@ from rest_framework import serializers
 from .models import Product, Review, Comment, Like, Follower
 from django.contrib.auth.models import User
 
-
 # Product Serializer
 class ProductSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')  
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'category', 'price', 'created_at', 'owner'] 
-
+        fields = ['id', 'name', 'description', 'category', 'price', 'created_at', 'owner']
 
 # Review Serializer
 class ReviewSerializer(serializers.ModelSerializer):
@@ -20,7 +18,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ['id', 'content', 'rating', 'created_at', 'product', 'author']
 
-
 # Comment Serializer
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
@@ -29,15 +26,22 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'content', 'created_at', 'review', 'author']
 
-
 # Like Serializer
 class LikeSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    review = serializers.PrimaryKeyRelatedField(queryset=Review.objects.all())
 
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'review', 'author']
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        review = validated_data['review']
+        existing_like = Like.objects.filter(author=user, review=review).first()
+        if existing_like:
+            raise serializers.ValidationError({'detail': 'Você já curtiu esta review.'})
+        return Like.objects.create(author=user, review=review)
 
 # Follower Serializer
 class FollowerSerializer(serializers.ModelSerializer):
@@ -47,4 +51,3 @@ class FollowerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follower
         fields = ['id', 'follower', 'following']
-
